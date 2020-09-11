8063a1de343bf42aa3ea98b9d9a89beed9727ae7
yanrw1 <71109805+yanrw1@users.noreply.github.com>
2020-09-11 10:56:06 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
         Log.e(TAG, "onCreate: ");
+        Log.e(TAG, "onCreate: ");
     }
 }
